f16773e9d3181c7e9eed7c59177ac13c8e6758c6
MAGIC <Vitaka145@gmail.com>
2023-03-19 15:18:55 +0200

    Голосовая верефикация доработана!
    Co-authored-by: Vailin21 <Vailin21@users.noreply.github.com>
    Co-authored-by: Ivan Puchkov <john.theco.dev@gmail.com>

--- verefy.py
+++ verefy.py
@@ -2,17 +2,20 @@ import discord
 import asyncio
 from discord.utils import get
 from discord.ext import commands
+from discord import errors
 from pymongo import MongoClient
 from discord.ui import Button, View, Select
 from discord import ui, Interaction
 import configparser
+from typing import List
 
 conf = configparser.ConfigParser()
 conf.read('config.ini')
 
-# Забираем данные ...
-support_con = conf.get('Settings', 'Support')
+# Забираем данные из конфигурации (int-списки принудительно кастуем в List[int])
+support_con:List[int] = [int(x) for x in conf.get('Settings', 'Support').split(',')]
 prefix = conf.get('Settings', 'Prefix')
+v_c:List[int] = [int(x) for x in conf.get('Settings', 'v_c').split(',')]
 
 
 intents = discord.Intents().all()
@@ -21,6 +24,9 @@ cluster = MongoClient("mongodb://127.0.0.1:27017")
 collguild = cluster.argand.guild
 
 
+# Отключаем стандартные команды Discord
+client.remove_command('help')
+
 
 @client.event
 async def on_ready():
@@ -28,7 +34,6 @@ async def on_ready():
         post1 = {
             "_id": guild.id,
             "name": guild.name,
-            "support": [],
             "eco_rate": 1.00,
             "exp_rate": 1.00,
             "verify": True
@@ -37,48 +42,173 @@ async def on_ready():
             collguild.insert_one(post1)
     print(f"бот {client.user} успешно запущен и подключен")
 
-class VerifyView(View):
-    def __init__(self, member):
+
+class NoVerifiableMembers(Exception):
+    """
+    Bсключение для обработки случая, когда у нас нет пользователей для верификации
+    """
+    pass
+
+
+class MemberSelect(ui.Select):
+    """
+    Класс селекта для для выбора пользователя.
+    Базовый конструктор элемента, ничего больше.
+    """
+    def __init__(self, options):
+        super().__init__(
+            placeholder = "Выберите пользователя, которого хотите верифицировать",
+            min_values = 1,
+            max_values = 1,
+            options = options
+        )
+class SelectMemberView(View):
+    """
+    View-объект для отображения выпадающего меню с выбором пользователя для дальнейшей верификации.
+    При отсутствии пользователей для верификации выдаем исключение NoVerifiableMembers и обрываем исполнение.
+
+    Аргументы:
+    ----------
+    - guild - объект guild, который мы передаем из контекста вызова команды, требуется для получения объекта member выбранного пользователя
+    """
+    def __init__(self, author, guild):
         super().__init__()
-        self.member = member
-    
-    @discord.ui.button(label="Мужской", style=discord.ButtonStyle.primary, emoji='♂️')
-    async def male(self, button: discord.ui.Button, interaction: discord.Interaction):
-        role = discord.utils.get(self.member.guild.roles, name='♂️')
-        unvr = discord.utils.get(self.member.guild.roles, name='unverify')
-        await self.member.add_roles(role, reason='Выбор пола')
-        await self.member.remove_roles(unvr, reason="Пройдена верификация")
+
+        self.author = author
+        self.guild = guild
+        self.member = None
+        self.v_c = [1056206116469612646, 1086615962599575604, 1086616038289985547, 1086616040215150695, 1086616082284027904]
+        self.member_options = self.parse_voice_channel_members()
+
+        # Если не нашли пользователей - триггерим исключение
+        if len(self.member_options) == 0:
+            raise NoVerifiableMembers
+            self.stop()
+
+        # Создаем Select-объект и добавляем его в финальную View
+        # Принудительно присобачиваем в качестве callback'а функцию этого класса (self.callback)
+        self.select = MemberSelect(options=self.member_options)
+        self.select.callback = self.callback
+        self.add_item(self.select)
+
+    """
+    Callback, который вызывается на выбор пользователя из выпадающего меню
+    Сохраняет объект выбранного пользователя в себе, вызываем ивент on_verify_selected_member
+    для дальнейшего продвижения по процессу верификации
+    """
+    async def callback(self, interaction: discord.Interaction):
+        if self.author.id != interaction.user.id:
+            sobaka = self.guild.get_member(interaction.user.id)
+            await interaction.response.send_message(f"Убери руки от чужой формы, собака {sobaka.mention}")
+            return
+
+        member_id = self.select.values[0]
+        self.member = await self.guild.fetch_member(member_id)
+        client.dispatch("on_verify_selected_member")
         self.stop()
-        # await interaction.response.send_message(f"Вы успешно провели верефикацию пользователю {self.member.name}. Ему была роль ♂️")
-        # self.stop()
+
+    """
+    Вспомогательная функция для поиска верифицируемых пользователей, отдает список найденных пользователей
+    Обязательное требование - наличие роли unverify
+    """
+    def parse_voice_channel_members(self):
+        member_options = []
+        unvr = discord.utils.get(self.guild.roles, name='unverify').id
+
+        for voice in self.v_c:
+            ch = client.get_channel(voice)
+            for member in ch.members:
+                member_roles = [x.id for x in member.roles]
+                if unvr in member_roles:
+                    member_options.append(discord.SelectOption(label=member.name, value=member.id))
+
+        return member_options
+
+    """
+    Просто возвращает выбранного пользователя (требуется для следующей View с выбором пола)
+    """
+    def get_selected_member(self):
+        return self.member
+
+
+class VerifyView(View):
+    def __init__(self, ctx, member, author, guild):
+        super().__init__()
+        self.ctx = ctx
+        self.guild = guild
+        self.author = author
+        self.member = member
+
+    @discord.ui.button(label="Мужской", style=discord.ButtonStyle.primary, emoji='♂️')
+    async def male(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.author.id != interaction.user.id:
+            sobaka = self.guild.get_member(interaction.user.id)
+            await interaction.response.send_message(f"Убери руки от чужой формы, собака {sobaka.mention}")
+            return
+        
+        gender_role = discord.utils.get(self.member.guild.roles, name='♂️')
+        verified_role = discord.utils.get(self.member.guild.roles, name='verify')
+        unvr = discord.utils.get(self.member.guild.roles, name='unverify')
+        await self.member.add_roles(gender_role, reason='Выбор пола')
+        await self.member.add_roles(verified_role, reason='Пройдена верификация')
+        await self.member.remove_roles(unvr, reason="Пройдена верификация")
+        await interaction.response.send_message(f"{self.ctx.author.name} успешно провели верефикацию пользователю {self.member.name}. Ему была роль ♂️")
+        client.dispatch("on_verify_selected_member")
+        self.stop()
 
     @discord.ui.button(label="Женский", style=discord.ButtonStyle.primary, emoji='♀️')
-    async def female(self, button: discord.ui.Button, interaction: discord.Interaction):
-        role = discord.utils.get(self.member.guild.roles, name='♀️')
+    async def female(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.author.id != interaction.user.id:
+            sobaka = self.guild.get_member(interaction.user.id)
+            await interaction.response.send_message(f"Убери руки от чужой формы, собака {sobaka.mention}")
+            return
+        
+        gender_role = discord.utils.get(self.member.guild.roles, name='♀️')
+        verified_role = discord.utils.get(self.member.guild.roles, name='verify')
         unvr = discord.utils.get(self.member.guild.roles, name='unverify')
-        await self.member.add_roles(role, reason='Выбор пола')
+        await self.member.add_roles(gender_role, reason='Выбор пола')
+        await self.member.add_roles(verified_role, reason='Пройдена верификация')
         await self.member.remove_roles(unvr, reason="Пройдена верификация")
+        await interaction.response.send_message(f"{self.ctx.author.name} успешно провели верификацию пользователю {self.member.name}. Ей была роль ♀️")
+        client.dispatch("on_verify_selected_member")
         self.stop()
-        # await interaction.response.send_message(f"Вы успешно провели верификацию пользователю {self.member.name}. Ей была роль ♀️")
-        # self.stop()
+
+"""
+Кастомный ивент для бота, который управляет поведением формы верификации пользователя
+"""
+@client.event
+async def on_verify_selected_member(ctx):
+    pass
 
 @client.command(
     name="verify",
     aliases=["vf", "verefy", "veref", 'verif'],
-    brief="Выберите как взаимодействовать с пользователем",
-    usage=f"{prefix}vf @пользователь пол(М или Д)"
+    brief="Верефикация пользователей"
 )
 async def verify(ctx, member: discord.Member=None, gender:str=None):
-    if member is None:
-        await ctx.send("Вы не указали пользователя")
+    if ctx.author.id not in support_con:
         return
-    # if gender is None:
-    #     await ctx.send("Вы не указали пол пользователя")
-    #     return
+    
+    try:
+        # Отрисовываем селект с выбором пользователя
+        select_member_view = SelectMemberView(ctx.author, ctx.guild)
+        select_member_msg = await ctx.send("Выберите пользователя для верификации", view=select_member_view)
 
-    view = VerifyView(member)
-    await ctx.send(f"Выберите пол для пользователя {member.mention}", view=view)
+        # Ждем выполнения выбора пользователя и удаляем предыдущее сообщение
+        await client.wait_for("on_verify_selected_member")
+        await select_member_msg.delete()
 
+        # Добавляем выбор гендера для выбранного пользователя
+        member = select_member_view.get_selected_member()
+        verify_view = VerifyView(ctx, member, ctx.author, ctx.guild)
+        verify_view_msg = await ctx.send(f"Выберите пол для пользователя {member.mention}", view=verify_view)
+
+        # Ожидаем выбора пола и верификации, после чего удаляем сообщение
+        await client.wait_for("on_verify_selected_member")
+        await verify_view_msg.delete()
+    except NoVerifiableMembers:
+        await ctx.send("Не найдено пользователей для верификации")
+    
 
 # @client.command(
 #         name = "verify",
@@ -141,15 +271,14 @@ async def voice_verify(ctx, sum):
         
 @client.event
 async def on_voice_state_update(member, before, after):
-    v_c = [1056206116469612646, 1086615962599575604, 1086616038289985547, 1086616040215150695, 1086616082284027904]
     guild = member.guild.id
     datag = collguild.find_one({"_id": guild})
     af = after.channel.id
     if datag["verify"] == True:
         if after.channel is None:
-            return
+            print("AFTER CHANNEL IS NONE")
         if member.id in support_con:
-            return
+            print("MEMBER ID IN SUPPORT CONT")
         # Добавить проверку, что человек только зашел на канал, а не перешёл или выполнил условие
         if before.channel is None and af in v_c:
             print(f"Пользователь {member.name} зашел в {after.channel.name} и ждет верефикации")
